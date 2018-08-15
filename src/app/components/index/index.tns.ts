2b20cfc463e37b0eb9d8c5f96e125b878f600ae5
Mateusz Gozdek <mgozdekof@gmail.com>
2018-08-16 01:04:27 +0200

    index: add navigation button to open filters

--- src/app/components/index/index.tns.ts
+++ src/app/components/index/index.tns.ts
@@ -1,11 +1,11 @@
 import * as app from 'application';
 import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
 import { IndexCommon } from './index.common';
+import { ViewChild } from '@angular/core';
+import { IndexComponent } from './index.component';
 
 export class Index extends IndexCommon {
-  constructor() {
-    super();
-  }
+  @ViewChild('index') index: IndexComponent;
 
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>app.getRootView();
