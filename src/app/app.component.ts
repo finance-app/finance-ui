6316e4581838402de9ba57a71d8707367290ccf0
Mateusz Gozdek <mgozdekof@gmail.com>
2018-11-06 23:30:10 +0100

    Unify with what ng new --shared generates
    Rather than using custom build scheme.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 import { AuthService, SessionService, TimeframeService } from '@app/core';
 
 declare const require: any;
@@ -15,13 +14,8 @@ export class AppComponent {
   public navbarCollapsed: boolean = true;
 
   constructor(
-    private translate: TranslateService,
     public auth: AuthService,
     public timeframeService: TimeframeService,
     private sessionService: SessionService,
-  ) {
-    translate.setTranslation('en', require('../assets/i18n/en.json'));
-    translate.setDefaultLang('en');
-    translate.use('en');
-  }
+  ) {}
 }
