cc720a81785766774cbdb15ce0ebfcae4846b6f0
Mateusz Gozdek <mgozdekof@gmail.com>
2021-12-25 17:57:02 +0100

    Fix applying environment variables
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://your-api.your-domain.com',
-  whitelistedDomains: ['your-api.your-domain.com'],
-  blacklistedRoutes: ['your-api.your-domain.com/user_token'],
+  apiUrl: window['env']['apiUrl'] || 'http://your-api.your-domain.com',
+  whitelistedDomains: [window['env']['whitelistedDomains'] || 'your-api.your-domain.com'],
+  blacklistedRoutes: [window['env']['blacklistedRoutes'] || 'your-api.your-domain.com/user_token'],
   tokenCookie: 'token'
 };
